05ae076cb85875523ffa29ca2bbb745e07aac328
kishorpatil3038 <gurukrupa3038@gmail.com>
2024-03-31 22:54:15 +0530

    this is my 5th repo

---
+++ 5th.py
@@ -0,0 +1,11 @@
+
+#---exception handling
+
+def my_function(country = "india"):
+    print("i am from" + " " + country)
+
+my_function("germany")
+my_function("Washingtone DC")
+my_function("united kingdom")
+my_function()
+
